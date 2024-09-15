7a3779d969d2f5efa26fd62fc4401b456ffe1a57
Robert Zavala <rbrtzavala@gmail.com>
2024-09-15 18:00:54 -0500

    create form for updateInvoice and send data to database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,8 @@ const CreateInvoice = FormSchema.omit({
     date: true
 })
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true})
+
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -32,6 +34,25 @@ export async function createInvoice(formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
