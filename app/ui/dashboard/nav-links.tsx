79bfdeef828930dca9230e035532425759937485
Robert Zavala <rbrtzavala@gmail.com>
2024-09-15 12:22:01 -0500

    create route group for Loading and Page compoonents. Use Suspense componet to set streaming for RevenueChart and LatestInvoice to use ui skeletons

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import Link from 'next/Link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
