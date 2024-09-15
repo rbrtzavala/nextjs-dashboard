95ee8cc228d235351bf82612eec052a1b716944b
Robert Zavala <rbrtzavala@gmail.com>
2024-09-15 13:31:30 -0500

    fix missing prop for earch comoinent

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,7 +8,7 @@ import {
 } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search() {
+export default function Search( {placeholder}: {placeholder: string}) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
@@ -31,7 +31,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder='Search'
+        placeholder={placeholder}
         onChange={(e) => {
             handleSearch(e.target.value);
         }}
