6352ba001834b46417e5b0891b9765bf454572e7
Robert Zavala <rbrtzavala@gmail.com>
2024-09-15 19:38:42 -0500

    fix typo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,7 +14,7 @@ const FormSchema = z.object({
         .number()
         .gt(0, { message: 'Please enter an amount greater than $0.' }),
     status: z.enum(['pending', 'paid'], {
-        inavlid_error_type: 'Please select an invoice status.'
+        invalid_type_error: 'Please select an invoice status.'
     }),
     date: z.string()
 });
